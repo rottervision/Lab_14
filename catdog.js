c3df67ca88ba116aac8e9535287bc393ad13b31e
Dave Rotter <dave@rottervision.com>
2016-11-21 16:32:23 -0500

    lab_14_catdog

---
+++ catdog.js
@@ -0,0 +1,18 @@
+  var app = angular.module('catDogMod', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+      .when('/cat', {
+        controller: 'simpleCtrl',
+        templateUrl: 'cat.html'
+      })
+      .when('/dog', {
+        controller: 'simpleCtrl',
+        templateUrl: 'dog.html'
+      })
+  });
+
+  app.controller('simpleCtrl', function(){
+
+
+  });
